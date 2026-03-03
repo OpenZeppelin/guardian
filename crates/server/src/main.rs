bada9958664052a889063c2d5d60fcff642e58cd
Rick <rsulisthio@gmail.com>
2026-03-03 07:48:57 +0000

    chore: update canoicalization config

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -34,7 +34,7 @@ async fn main() {
     ServerBuilder::new()
         .with_logging(LoggingConfig::default())
         .network(NetworkType::MidenDevnet)
-        .with_canonicalization(Some(CanonicalizationConfig::new(10, 24)))
+        .with_canonicalization(None)
         .with_rate_limit(RateLimitConfig::from_env())
         .with_body_limit(BodyLimitConfig::from_env())
         .storage(storage_backend)
